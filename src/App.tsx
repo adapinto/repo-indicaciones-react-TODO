7f4f822696e873e9d1b00a5df83ade9cd4e7e09c
Uayeb Caballero <uayeb.caballero@gmail.com>
2023-04-25 20:54:50 -0600

    TODO completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,95 @@
-import React from 'react';
+import { useState , useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { ITask } from './interfaces/ITask' 
+
+import TaskForm from './components/TaskForm'
+import Panel from './components/Panel'
+
+
 function App() {
+
+  const [id, setId] = useState<number>(0)
+  const [task, setTask] = useState<ITask>({ "status" : "TODO", "id": 0 })
+  const [taskList, setTaskList] = useState<ITask[]>([])
+  const [teams, setTeams] = useState<string[]>(["Development", "QA", "PMs", "BI"])
+  
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTask({...task, [e.target.name]: e.target.value})
+  }
+
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setTask({...task, [e.target.name]: e.target.value})
+  }
+
+  const changeStatus = (id: number, status: string) => {
+    const newTaskList = taskList.map( task => {
+      if(task.id === id) {
+        task.status = status
+      }
+      return task
+    })
+    setTaskList(newTaskList)
+  }
+
+  const deleteTask = (id: number) => {
+    const newTaskList = taskList.filter( task => task.id !== id )
+    setTaskList(newTaskList)
+  }
+
+  const addTask = () => {   
+    
+    setTaskList([...taskList, task])
+    const newId: number = id + 1
+    setId(newId)
+
+    setTask({ 
+      "id" : newId
+      , "status" : "TODO" 
+      , "name" : "" 
+      , "team" : "" 
+      , "hours" : 0 
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <header>
+          <h1>TODO List</h1>
       </header>
+
+      <div className="container">
+        <TaskForm 
+            task={task} 
+            teams={teams} 
+            onChangeInput={handleInputChange}
+            onChangeSelect={handleSelectChange}
+            onSave={addTask}             
+        />
+        <div className="columnas">
+          <Panel 
+            title={"Tareas Pendientes"} 
+            tasks={ taskList.filter( task => task.status === 'TODO' ) }
+            changeStatus={changeStatus}
+            deleteTask={deleteTask}
+          />
+          <Panel 
+            title={"Tareas en progreso"} 
+            tasks={ taskList.filter( task => task.status === 'In Progress' ) }
+            changeStatus={changeStatus}
+            deleteTask={deleteTask}
+          />
+          <Panel 
+            title={"Tareas Completadas"} 
+            tasks={ taskList.filter( task => task.status === 'Completed' ) }
+            changeStatus={changeStatus}
+            deleteTask={deleteTask}
+          />
+        </div>
+      </div>
     </div>
   );
 }
